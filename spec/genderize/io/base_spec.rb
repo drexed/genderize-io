9b2fe8d1902c04c16ea9686c1b65a406d68c15b4
Juan Gomez <j.gomez@drexed.com>
2020-11-06 19:01:32 -0500

    Linter fixes

--- spec/genderize/io/base_spec.rb
+++ spec/genderize/io/base_spec.rb
@@ -3,42 +3,42 @@
 require 'spec_helper'
 
 RSpec.describe Genderize::Io::Base do
-  let(:klass_1) { described_class.new('kim') }
-  let(:klass_2) { described_class.new('kim', country_id: 'dk', language_id: 'en') }
+  let(:klass1) { described_class.new('kim') }
+  let(:klass2) { described_class.new('kim', country_id: 'dk', language_id: 'en') }
 
   describe '.initialize' do
     it 'returns "kim"' do
-      expect(klass_1.name).to eq('kim')
+      expect(klass1.name).to eq('kim')
     end
 
     it 'returns "https://api.genderize.io"' do
-      expect(klass_1.host).to eq('https://api.genderize.io')
+      expect(klass1.host).to eq('https://api.genderize.io')
     end
 
     it 'returns nil' do
-      expect(klass_1.country_id).to eq(nil)
-      expect(klass_1.language_id).to eq(nil)
-      expect(klass_1.request).to eq(nil)
+      expect(klass1.country_id).to eq(nil)
+      expect(klass1.language_id).to eq(nil)
+      expect(klass1.request).to eq(nil)
     end
   end
 
   describe '.param_country_id' do
     it 'returns nil' do
-      expect(klass_1.send(:param_country_id)).to eq(nil)
+      expect(klass1.send(:param_country_id)).to eq(nil)
     end
 
     it 'returns "&country_id=dk"' do
-      expect(klass_2.send(:param_country_id)).to eq('&country_id=dk')
+      expect(klass2.send(:param_country_id)).to eq('&country_id=dk')
     end
   end
 
   describe '.param_language_id' do
     it 'returns nil' do
-      expect(klass_1.send(:param_language_id)).to eq(nil)
+      expect(klass1.send(:param_language_id)).to eq(nil)
     end
 
     it 'returns "&language_id=en"' do
-      expect(klass_2.send(:param_language_id)).to eq('&language_id=en')
+      expect(klass2.send(:param_language_id)).to eq('&language_id=en')
     end
   end
 
--- spec/genderize/io/lookup_spec.rb
+++ spec/genderize/io/lookup_spec.rb
@@ -3,8 +3,8 @@
 require 'spec_helper'
 
 RSpec.describe Genderize::Io::Lookup do
-  let(:klass_1) { described_class.new('kim') }
-  let(:klass_2) do
+  let(:klass1) { described_class.new('kim') }
+  let(:klass2) do
     VCR.use_cassette('lookup') do
       described_class.determine('kim')
     end
@@ -12,13 +12,13 @@ RSpec.describe Genderize::Io::Lookup do
 
   describe '.initialize' do
     it 'returns {}' do
-      expect(klass_1.data).to eq({})
+      expect(klass1.data).to eq({})
     end
   end
 
   describe '.determine' do
     it 'returns true for count key' do
-      expect(klass_2.data.key?('count')).to eq(true)
+      expect(klass2.data.key?('count')).to eq(true)
     end
   end
 
@@ -27,7 +27,7 @@ RSpec.describe Genderize::Io::Lookup do
     opts_url = 'https://api.genderize.io?api_key=xxx&name=kim&country_id=dk&language_id=en'
 
     it "returns '#{base_url}'" do
-      expect(klass_1.url).to eq(base_url)
+      expect(klass1.url).to eq(base_url)
     end
 
     it "returns '#{opts_url}'" do
@@ -45,11 +45,11 @@ RSpec.describe Genderize::Io::Lookup do
   Genderize::Io::Lookup::HEADER_KEYS.each do |key|
     describe ".#{key}" do
       it 'returns nil' do
-        expect(klass_1.send(key)).to eq(nil)
+        expect(klass1.send(key)).to eq(nil)
       end
 
       it 'returns an integer' do
-        expect(klass_2.send(key).is_a?(Integer)).to eq(true)
+        expect(klass2.send(key).is_a?(Integer)).to eq(true)
       end
     end
   end
@@ -57,18 +57,18 @@ RSpec.describe Genderize::Io::Lookup do
   Genderize::Io::Lookup::RESPONSE_KEYS.each do |key|
     describe ".#{key}" do
       it 'returns nil' do
-        expect(klass_1.send(key)).to eq(nil)
+        expect(klass1.send(key)).to eq(nil)
       end
 
       it "returns value in response_hash['#{key}']" do
-        expect(klass_2.send(key)).to eq(response_hash[key])
+        expect(klass2.send(key)).to eq(response_hash[key])
       end
     end
   end
 
   describe '.param_name' do
     it 'returns "name=kim"' do
-      expect(klass_1.send(:param_name)).to eq('name=kim')
+      expect(klass1.send(:param_name)).to eq('name=kim')
     end
   end
 
